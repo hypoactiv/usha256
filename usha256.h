5f5a9aa0fd03670dd20fb4bd340748e8caab4259
Jonathan Schroeder <jd.schroeder@gmail.com>
2017-07-10 22:52:19 -0400

    initial commit

---
+++ usha256.h
@@ -0,0 +1,21 @@
+#include "stdint.h"
+
+typedef struct {
+  uint8_t datalen;
+  union {
+    uint32_t bitlen;
+    uint8_t bitlen_bytes[4];
+  };
+  union {
+    uint32_t state[8];
+    uint8_t state_bytes[64];
+  };
+  uint8_t data[64];
+} Sha256Context;
+
+// Initialize a SHA256 context
+void sha256_init(Sha256Context *ctx);
+// Update SHA256 hash with new message data
+void sha256_update(Sha256Context *ctx, uint8_t *data, uint8_t len);
+// Finalize and store SHA256 hash in hash
+void sha256_final(Sha256Context *ctx, uint8_t hash[]);
