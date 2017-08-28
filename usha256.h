1c62a3f6e7c66b16d84db5940c4e01e5cc0a6d41
Jonathan Schroeder <jd.schroeder@gmail.com>
2017-08-28 00:19:47 -0400

    protect header with ifndef

--- usha256.h
+++ usha256.h
@@ -1,3 +1,6 @@
+#ifndef USHA256_H
+#define USHA256_H
+
 #include "stdint.h"
 
 typedef struct {
@@ -22,3 +25,5 @@ void sha256_init(Sha256Context *ctx);
 void sha256_update(Sha256Context *ctx, uint8_t *data, uint16_t len);
 // Finalize and store SHA256 hash in hash
 void sha256_final(Sha256Context *ctx, uint8_t hash[]);
+
+#endif // USHA256_H
